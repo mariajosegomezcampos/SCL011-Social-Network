48d2e5a633fb30a77c1da761c9356aea19218d77
Mariajose <mariajosegomezcampo@gmail.com>
2019-10-11 13:27:57 -0400

    bajando cambio

--- src/main.js
+++ src/main.js
@@ -1,7 +1,34 @@
+
 // local Storage :guarda cadena de texto a traves de una clave y su respectivo valor
 // Set => Guardando
- // Get =>obtener 
- 
+ // Get =>obtener
+// variables globales
+const emailLogin = document.getElementById("emaiLogin");
+const passwordLogin = document.getElementById("passwordLogin");
+const loginButton = document.getElementById("loginButton");
 
 
+const nameUser = document.getElementById("nameUser");
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const confirPassword = document.getElementById('confirPassword');
+const errorPassword = document.getElementById('errorPassword');
+const registerButton = document.getElementById('registerButton');
+
+
+//  contenedor donde se imprime
+const logContainer = document.getElementById("logContainer");
+
+// declaro mis variables globales
+const registerLink = document.querySelector("#registerLink");
+const formRegister = document.querySelector("#formRegister");
+
+(()=>{
+    registerLink.addEventListener("click",agregrarFormRegister);
+} )();
+
+function agregrarFormRegister(e){
+e.preventDefault();
+formRegister.innerHTML
+}
 
--- src/views/login.js
+++ src/views/login.js
@@ -1,12 +1,37 @@
 // declaro mis variables
-const emailLogin = document.getElementById("email-login").value
-const passwordLogin = document.getElementById("password-login").value
-const loginButton = document.getElementById("loginButton");
-const registerButton = document.getElementById('registerButton');
 
-const nameUser = document.getElementById("nameUser");
-const email = document.getElementById('email');
-const password = document.getElementById('password');
-const confirPassword = document.getElementById('confirPassword')
-const errorPassword = document.getElementById('error-password');
+// aqui se crean mis div dinamicos 
+// Formulario de Login
 
+logContainer.innerHTML= `
+ <div id="container">
+          <section class="section-forms">
+            <div id="formInicio">
+                <img src="" class="logo-Scity" alt="logo-santiagocity"> 
+                <div>
+                  <h4 class="slogan-login">¡Tips para disfrutar mejor!</h4>
+                  <form class="">
+                    <div class="">
+                      <input id="email-login" type="email" class="validate">
+                      <label for="email-login">Email</label>
+                    </div>
+                    <div class="">
+                      <input id="password-login" type="password" class="validate">
+                      <label for="password-login">Contraseña</label>
+                      <div><a href="#" class="update-password"><p id="update-password">¿Olvidó su contraseña?</p></a></div>
+                    </div>
+                    <div>
+                      <input type="button" id="loginButton" class="" value="Ingresa">
+                    </div>  
+                  </form>
+                </div>
+                <br>
+                <div>
+                  <div><p class="go-register">¿Todavía no eres miembro?</p><a href="#" id="registerLink" class="link-to-register"> Únete ahora</a></div>
+                </div>
+            </div> `; 
+            
+            console.log("")
+                
+            
+ 
