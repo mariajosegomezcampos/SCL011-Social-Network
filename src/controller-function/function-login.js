7740e8d12989424ed3812f182508d93b1ed175b0
Mariajose <mariajosegomezcampo@gmail.com>
2019-10-18 18:25:07 -0400

    creado condicional dentro de router,completando funcion login

--- src/controller-function/function-login.js
+++ src/controller-function/function-login.js
@@ -1,51 +1,24 @@
-//logica del negocio
-//realizo funcion 
-import{iniciarSesion}from './views/login.js';
- export function obtenerListaUsuario(){
-    const listaUsuario = JSON.parse(localStorage.getItem('listadeUsuarioLs'));
-    if(listaUsuario == null){
-        listaUsuario =
-        [
-            // id nombre apellido correo contraseña fecha de nacimiento
-// usuarios registrados
-        ["1","pablo","monastel","pablomonastel@gmail.com","1pablo9","08-08-1990"],
-        ["2","pedro","campos","pedrocampos@gmail.com","1pedro9","09-09-1992"],
-       ]
-    }
-    return listaUsuario;
-}
-//     document.querySelector("btnIngresar").addEventListener("click",iniciarSesion);
-//     console.log("funciona boton ingresar");
+import{validarCredenciales}from './js/auths.js'
 
-//    export function iniciarSesion(){
+ 
 
 
-//    const sCorreo ='';
-//    const sContraseña ='';
-//    const bAcceso = false;
-//    sCorreo= document.querySelector( '#emailLogin').Value;
-//    sContraseña= document.querySelector('#passwordLogin').Value;
-//    bAcceso= validarcredenciales(sCorreo,sContraseña);
-//     console.log('bAcceso');
-//    }
+// export const login = () => {
+// const email ='';
+// const password ='';
+// const acceso = false;
+// document.getElementById("btnIngresar").addEventListener('click', () => {
+//  console.log("funciona boton ingresar");
+//  email = document.getElementById("email-login").value;
+//  password = document.getElementById("password-login").value;
+//  acceso = validarCredenciales(email,password);
+//  acceso = true;
+//  console.log('acceso');
+// });
+
 // }
+// return listaUsuario;
 
-//     return listaUsuario;
-// realizo funcion que me va indciar si la persona puede iniciar sesion o no
-// le paso por parametro correo y contraseña
-     export function validarCredenciales(pCorreo , pContraseña){
-        const listaUsuario = obtenerListaUsuario();
-        const bAcceso = false;
-        for ( [i] = 0; [i] < listaUsuario.length; i++) {
-            if(pCorreo == listaUsuario[i][3]&& pContraseña == listaUsuario[i][4]){
-                bAcceso = true;
-                sessionStorage.setItem('usuarioactivo',listaUsuario[i][1]+''+listaUsuario[i][2]);
-            }
-        }
-
-        return bAcceso;
-        
-    }
 
 
 
---
+++ src/js/auth.js
@@ -0,0 +1,51 @@
+// //logica del negocio
+
+
+ export function obtenerListaUsuario(){
+    //  aca hago q deje de ser un string
+ const listaUsuario = JSON.parse(localStorage.getItem('listadeUsuarioLs'));
+    if(listaUsuario == null){
+         listaUsuario =
+         [
+                // id nombre apellido correo contraseña fecha de nacimiento
+// usuarios registrados
+ ["1","pablo","monastel","pablomonastel@gmail.com","1pablo9","08-08-1990"],
+  ["2","pedro","campos","pedrocampos@gmail.com","1pedro9","09-09-1992"],
+     ]
+   }
+     console.log(" listaUsuario");
+ }
+
+// // realizo funcion que me va indciar si la persona puede iniciar sesion o no
+// // le paso por parametro correo y contraseña
+  export function validarCredenciales(email, password){
+    const listaUsuario = obtenerListaUsuario();
+    const acceso = false;
+     for ( [i] = 0; [i] < listaUsuario.length; i++) {
+           if(email== listaUsuario[i][3]&& password== listaUsuario[i][4]){
+                            acceso = true;
+                                        sessionStorage.setItem('usuarioactivo',listaUsuario[i][1]+''+listaUsuario[i][2]);
+            }
+        }
+
+        return acceso;
+        
+    }
+
+
+
+
+
+
+// guardar_localStorage();
+
+
+
+
+
+
+
+
+
+
+
