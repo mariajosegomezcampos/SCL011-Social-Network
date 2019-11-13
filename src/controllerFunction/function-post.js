72d70562529c9b4447600b3119c0c94021bbdd0c
macarenasaez <makadaira@gmail.com>
2019-11-12 23:00:54 -0300

    imprimiendo los posts

--- src/controllerFunction/function-post.js
+++ src/controllerFunction/function-post.js
@@ -3,26 +3,33 @@ export const publishpost = () =>{
     let getpost =document.getElementById("textpost").value;
 
     var db = firebase.firestore();
+
     db.collection("Posts").add({
         newcomemt:getpost,
     })
+
     .then(function(docRef) {
         console.log("Document written with ID: ", docRef.id);
+        document.getElementById("textpost").value ='';
     })
     .catch(function(error) {
         console.error("Error adding document: ", error);
     });
 }
 
+//mostrar los posts
     export const allposts = () =>{
 
         var db = firebase.firestore();
         
-        let showAllPost =document.getElementById("toPost");
+       // let showAllPost =document.getElementById("toPost");
         db.collection("Posts").get().then((querySnapshot) => {
+            document.getElementById("root").innerHTML = '';
             querySnapshot.forEach((doc) => {
 
                 console.log(`${doc.id} => ${doc.data().newcomemt}`);
+                publishpost(doc);
+
                 showAllPost.innerHTML+= `
                 <div>${doc.data().newComemt}</div>
                 `
@@ -30,35 +37,17 @@ export const publishpost = () =>{
         });
     }
    
+    /*export function deletep (id){
+        var db = firebase.firestore();
+        db.collection("Posts").doc(id).delete().then(function() {
+            console.log("Document successfully deleted!");
+        }).catch(function(error) {
+            console.error("Error removing document: ", error);
+        });
+
+    }*/
 
 
 
-   /*db.collection("users").add({
-    first: "Ada",
-    last: "Lovelace",
-    born: 1815
-})
-.then(function(docRef) {
-    console.log("Document written with ID: ", docRef.id);
-})
-.catch(function(error) {
-    console.error("Error adding document: ", error);
-});
-    var db = firebase.firestore();
-    db.collection("users").add({
-        first: "publishpost",
-    
-    })
-    .then(function(docRef) {
-        console.log("Document written with ID: ", docRef.id);
-        document.getElementById('text').value =""
-    })
-    .catch(function(error) {
-        console.error("Error adding document: ", error);
-    });
- 
-      
-
- }*/
 
     
--- src/views/post.js
+++ src/views/post.js
@@ -1,19 +1,37 @@
 import { publishpost } from "../controllerFunction/function-post.js"
+//import{SignOff} from '../controllerFunction/functionLogin.js';
 
 export const templatePost = () =>{
     const containerPost = document.createElement('div');
 
     const contentPost = ` <h3>Recomendando lugares de Santigo!!</h3>
     <textarea id="textpost" rows="10" cols="70" placeholder="dejanos tus post" ></textarea>
-                               <button  id="toPost">publicar</button>
-                               <button id="like">like</button>`
+                               <button  id="toPost">publicar</button>`
 
      containerPost.innerHTML =  contentPost;
       document.getElementById('root').appendChild(containerPost).innerHTML;
 
+//Boton para publicar
  const toPostbtn  = document.getElementById("toPost");
  toPostbtn.addEventListener("click" , () =>{
    publishpost();
- })
+ });
  return containerPost;
 }
+
+
+/*Imprimir post
+export const print = (doc) => {
+  document.getElementById("root").innerHTML += `
+   <div id="allPosts">${doc.data().newComment}</div>
+   <p id=emailPost>${doc.data().userId}</p>
+   <button id=deletePost>Borrar</button>
+       `
+let getPost = doc.id
+console.log(getPostId)
+const deleteBtn = document.getElementById("deletePost");
+deleteBtn.addEventListener('click', () => {
+  deletePost(getPost);
+      })
+}*/
+
